a5ecd0b61b0e0aaec72f508e2d10d59f43d98dee
ataeri <atanuman@gmail.com>
2020-06-09 23:16:48 -0400

    added countdown

--- script.js
+++ script.js
@@ -49,3 +49,31 @@ const reasonToLove = () => {
 var image = "image" + Math.floor(Math.random() * 11) + ".jpg"
 document.body.style.backgroundImage = "url(images/" + image + ")"
 console.log(image)
+
+var countDownDate = new Date("Jul 11, 2020 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
